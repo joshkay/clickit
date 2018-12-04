22d24f5ff053ec918fc9dcb79322350d045d0759
Josh Kay <josh.m.kay@gmail.com>
2018-12-03 23:28:48 -0500

    Assignment 4: completed

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -3,5 +3,9 @@ module.exports =
   index(req, res, next)
   {
     res.render('static/index', {title: "Welcome to Bloccit"});
+  },
+  about(req, res, next)
+  {
+    res.render('static/about');
   }
 }
