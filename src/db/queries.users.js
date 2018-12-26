4c91e045470fcf72551ad5eca16011ad17bccf39
Josh Kay <josh.m.kay@gmail.com>
2018-12-26 11:23:27 -0500

    Checkpoint 11: completed

---
+++ src/db/queries.users.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcryptjs');
+
+const User = require('./models').User;
+
+module.exports =
+{
+  createUser(newUser, callback)
+  {
+    const salt = bcrypt.genSaltSync();
+    const hashedPassword = bcrypt.hashSync(newUser.password, salt);
+
+    return User.create({
+      email: newUser.email,
+      password: hashedPassword
+    })
+    .then((user) =>
+    {
+      callback(null, user);
+    })
+    .catch((err) =>
+    {
+      callback(err);
+    });
+  }
+};
